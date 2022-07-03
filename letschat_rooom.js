d96477cc7777dac47a3e8f739f90c6cbf38c6da9
NadiyaFathima <95966168+NadiyaFathima@users.noreply.github.com>
2022-07-03 16:23:46 +0530

    Add files via upload

---
+++ letschat_rooom.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBtwZHHBg89I4k3ZVndaSoZaMoeY5yQPjI",
+    authDomain: "letschat-debaf.firebaseapp.com",
+    databaseURL: "https://letschat-debaf-default-rtdb.firebaseio.com",
+    projectId: "letschat-debaf",
+    storageBucket: "letschat-debaf.appspot.com",
+    messagingSenderId: "1088548066882",
+    appId: "1:1088548066882:web:145f09508eacac13307fdb"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ 
